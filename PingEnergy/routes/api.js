3c1f3541838819b9429e22ce8e037e0c4f20358f
LithiaH <lithiah@users.noreply.github.com>
2016-04-10 11:12:58 -0400

    give each route own page

--- PingEnergy/routes/api.js
+++ PingEnergy/routes/api.js
@@ -5,18 +5,12 @@ var express = require('express'),
 var router = express.Router();
 var cache = apicache.middleware;
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Ping Energy', buildings: ["Beard", "Chapin", "Clark", "Cragin", "Everett", "Gebbie", "Keefe", "Kilham", "Larcom", "McIntire", "Meadows", "Metcalf", "Stanton", "White", "Young"]});
-});
-
-router.get('/api/:building', cache('1 minute'), function(req, res, next) {
+router.get('/:building', cache('1 minute'), function(req, res, next) {
     var building = req.params.building;
     console.log("\nRequest New Data For Building: "+building);
     request('http://egauge-'+building+'.wheatoncollege.edu/cgi-bin/egauge?tot', function (error, response, body) {
         res.send(body);
     });
 });
-
 
 module.exports = router;
