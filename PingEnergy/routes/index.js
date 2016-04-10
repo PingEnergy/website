4ae5b1e2a11257cc248e028535746633bf1f15f8
LithiaH <lithiah@users.noreply.github.com>
2016-04-10 15:50:16 -0400

    install mathjs for rounding and generate bubbles ranked list from data

--- PingEnergy/routes/index.js
+++ PingEnergy/routes/index.js
@@ -1,75 +1,30 @@
 var express = require('express'),
     apicache = require('apicache'),
     cheerio = require('cheerio'),
-    request = require('request');
+    request = require('request'),
+    math = require('mathjs');
+
 var router = express.Router();
 var cache = apicache.middleware;
-
-/* GET home page. */
-// var jsbObject = { "name": "buildings",
-//     "children": [
-//         {"name": "1st",
-//             "children": [
-//                 {"name": "Beard", "size": 10000, "active": false, "money": "$86.40"}
-//             ]
-//         },
-//         {"name": "2nd",
-//             "children": [
-//                 {"name": "Chapin", "size": 8500, "active": false, "money": "$80.64"}
-//             ]
-//         },
-//         {"name": "3rd",
-//             "children": [
-//                 {"name": "Clark", "size": 7000, "active": false, "money": "$74.88"}
-//             ]
-//         },
-//         {"name": "2ndgroup",
-//             "children": [
-//                 {"name": "Cragin", "size": 6500, "active": false, "money": "$69.12"},
-//                 {"name": "Everett", "size": 6250, "active": false, "money": "$63.36"},
-//                 {"name": "Gebbie", "size": 5500, "active": false, "money": "$57.60"}
-//             ]
-//         },
-//         {"name": "3rdgroup",
-//             "children": [
-//                 {"name": "Keefe", "size": 5000, "active": false, "money": "$51.84"},
-//                 {"name": "Kilham", "size": 4500, "active": false, "money": "$46.08"},
-//                 {"name": "Larcom", "size": 3800, "active": false, "money": "$40.32"}
-//             ]
-//         },
-//         {"name": "4thgroup",
-//             "children": [
-//                 {"name": "McIntire", "size": 3750, "active": false, "money": "$34.56"},
-//                 {"name": "Meadows", "size": 3500, "active": false, "money": "$28.80"},
-//                 {"name": "Metcalf", "size": 3250, "active": false, "money": "$23.04"}
-//             ]
-//         },
-//         {"name": "5thgroup",
-//             "children": [
-//                 {"name": "Stanton", "size": 3000, "active": false, "money": "$17.28"},
-//                 {"name": "White", "size": 2750, "active": false, "money": "$11.52"},
-//                 {"name": "Young", "size": 2500, "active": false, "money": "$5.76"}
-//             ]
-//         }
-//     ]
-// };
 
 buildings = [{"Beard": 60.3}, {"Chapin": 58.2}, {"Clark": 43.5}, {"Cragin": 38}, {"Everett": 37}, {"Gebbie": 34}, {"Keefe": 30}, {"Kilham": 21}, {"Larcom": 19}, {"McIntire": 18.76}, {"Meadows": 18}, {"Metcalf": 17.9}, {"Stanton": 17.8}, {"White": 15}, {"Young": 2}];
 
 //assuming buildings come with buildings IN ORDER from most to least ENERGY SAVED
 function createBuildingObject(buildings) {
+    //constants
     var jsb = { "name": "buildings", "children" : []};
     var groupNames = ["1st", "2nd", "3rd", "2ndgroup", "2ndgroup", "2ndgroup", "3rdgroup", "3rdgroup", "3rdgroup", "4thgroup", "4thgroup", "4thgroup", "5thgroup", "5thgroup", "5thgroup"];
     var sizeScale = 1500;
     var moneyScale = .14;
     var carbonScale = .6379;
 
+    //loop buildings to create object
     for (i = 0; i < buildings.length; i++) {
         var kwh = buildings[i][Object.keys(buildings[i])];
 
-        var size = kwh * sizeScale;
-        var money = kwh * moneyScale;
-        var c02 = kwh * carbonScale;
+        var size = math.round((kwh * sizeScale) * 100)/100;
+        var money = math.round((kwh * moneyScale) * 100)/100;
+        var c02 = math.round((kwh * carbonScale) * 100)/100;
 
         var child = {"name": groupNames[i],
             "children": [
@@ -83,11 +38,23 @@ function createBuildingObject(buildings) {
     return jsb;
 }
 
+function createBuildingList(buildings) {
+    var listBuildings = [];
+
+    for (i = 0; i < buildings.length; i++) {
+        listBuildings.push(Object.keys(buildings[i])[0]);
+    }
+
+    return listBuildings;
+}
+
 var jsbObject = createBuildingObject(buildings);
+var listBuildings = createBuildingList(buildings);
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Ping Energy', listBuildings: ["Beard", "Chapin", "Clark", "Cragin", "Everett", "Gebbie", "Keefe", "Kilham", "Larcom", "McIntire", "Meadows", "Metcalf", "Stanton", "White", "Young"], data: JSON.stringify(jsbObject)});
+  res.render('index', { title: 'Ping Energy', listBuildings: listBuildings, data: JSON.stringify(jsbObject)});
 });
 
+//
 
 module.exports = router;
