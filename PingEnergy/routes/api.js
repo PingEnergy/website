b538b091b92b56914c825abf21fd878941e9c4b7
Qi Zhang <zhang_qi@wheatoncollege.edu>
2016-04-18 19:31:59 -0400

    Add xml2js

--- PingEnergy/routes/api.js
+++ PingEnergy/routes/api.js
@@ -1,16 +1,28 @@
 var express = require('express'),
     apicache = require('apicache'),
     cheerio = require('cheerio'),
-    request = require('request');
+    request = require('request'),
+    parseString = require('xml2js').parseString;
+
 var router = express.Router();
 var cache = apicache.middleware;
 
 router.get('/:building', cache('1 minute'), function(req, res, next) {
     var building = req.params.building;
     console.log("\nRequest New Data For Building: "+building);
-    request('http://egauge-'+building+'.wheatoncollege.edu/cgi-bin/egauge?tot', function (error, response, body) {
-        res.send(body);
-    });
+
+    var num = parseInt("0x57100854", 16);
+    console.log(num);
+    var d = new Date(num*1000);
+    // d.setUTCMilliseconds(num);
+    console.log(d);
+    // request('http://cs.wheatoncollege.edu/~egauge/'+building+'/'+building+'.xml', function (error, response, body) {
+    //     parseString(body, function (err, result) {
+    //         // console.dir(result);
+    //         res.json(result);
+    //     });
+
+    // });
 });
 
 module.exports = router;
