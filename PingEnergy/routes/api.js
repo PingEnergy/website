3f0d3814109860c019cde7082cdcb4f249d9d5e2
LithiaH <lithiah@users.noreply.github.com>
2016-04-17 20:17:33 -0400

    convert server data to json object

--- PingEnergy/routes/api.js
+++ PingEnergy/routes/api.js
@@ -2,15 +2,29 @@ var express = require('express'),
     apicache = require('apicache'),
     cheerio = require('cheerio'),
     request = require('request');
+    xml2js = require('xml2js');
 var router = express.Router();
 var cache = apicache.middleware;
 
 router.get('/:building', cache('1 minute'), function(req, res, next) {
     var building = req.params.building;
     console.log("\nRequest New Data For Building: "+building);
-    request('http://egauge-'+building+'.wheatoncollege.edu/cgi-bin/egauge?tot', function (error, response, body) {
-        res.send(body);
-    });
+
+    request('http://cs.wheatoncollege.edu/~egauge/' + building + '/'  + building + '.xml',
+        function (error, response, body) {
+        //     res.send(body);
+
+            var parseString = xml2js.parseString;
+            var xml = body;
+
+            parseString(xml, function (err, result) {
+                console.dir(result);
+                res.json(result);
+            });
+
+        });
+
+
 });
 
 module.exports = router;
