05b49e94b0bf556a88370d0019d2df11666f3b0b
LithiaH <lithiah@users.noreply.github.com>
2016-04-18 19:30:09 -0400

    add mongodb to app

--- PingEnergy/routes/api.js
+++ PingEnergy/routes/api.js
@@ -17,9 +17,19 @@ router.get('/:building', cache('1 minute'), function(req, res, next) {
             var parseString = xml2js.parseString;
             var xml = body;
 
-            parseString(xml, function (err, result) {
+            parseString(xml,
+            function (err, result) {
                 console.dir(result);
-                res.json(result);
+                console.dir(result["group"]["data"][0]["$"]);
+
+                var timestamp = result["group"]["data"][0]["$"]["time_stamp"];
+
+                var date = new Date();
+                date.setTime(parseInt(timestamp, 16) * 1000); //start date of data
+
+                console.dir(result["group"]["data"][0]["r"][0]["c"]); //first minute data
+
+                console.dir(JSON.stringify(result));
             });
 
         });
