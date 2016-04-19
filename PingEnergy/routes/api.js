70bf9ef46c67f0cf6b5a363e1bb425760adcb6cf
LithiaH <lithiah@users.noreply.github.com>
2016-04-18 20:25:38 -0400

    get chapin data into database

--- PingEnergy/routes/api.js
+++ PingEnergy/routes/api.js
@@ -19,17 +19,67 @@ router.get('/:building', cache('1 minute'), function(req, res, next) {
 
             parseString(xml,
             function (err, result) {
-                console.dir(result);
-                console.dir(result["group"]["data"][0]["$"]);
+                // // console.dir(result);
+                // // console.dir(result["group"]["data"][0]["$"]);
+                //
+                // var timestamp = result["group"]["data"][0]["$"]["time_stamp"];
+                //
+                // var date = new Date();
+                // date.setTime(parseInt(timestamp, 16) * 1000); //start date of data
+                //
+                // console.dir(result["group"]["data"][0]["r"][0]["c"]); //first minute data
+                //
+                // // console.dir(JSON.stringify(result));
 
-                var timestamp = result["group"]["data"][0]["$"]["time_stamp"];
+                var raw = result;
+                var newjson = {"building": "Chapin",
+                            "startTime": 1,
+                            "endTime": 5,
+                            "timeInterval": 60,
+                            "energyUsage": []
+                        }
 
-                var date = new Date();
-                date.setTime(parseInt(timestamp, 16) * 1000); //start date of data
+                newjson["startTime"] = parseInt(result["group"]["data"][0]["$"]["time_stamp"], 16) * 1000;
+                newjson["endTime"] = parseInt(result["group"]["data"][0]["$"]["epoch"], 16) * 1000;
 
-                console.dir(result["group"]["data"][0]["r"][0]["c"]); //first minute data
+                newjson["timeInterval"] = result["group"]["data"][0]["$"]["time_delta"];
 
-                console.dir(JSON.stringify(result));
+                var newTime = newjson["startTime"];
+
+                console.log("newTime: ", newTime);
+
+                for (i in result["group"]["data"][0]["r"]) {
+
+                    newTime = newTime.toString();
+
+                    newRow = {};
+                    newRow[newTime] = result["group"]["data"][0]["r"][i]["c"][0];
+
+                    newjson["energyUsage"].push( newRow );
+
+                    newTime = parseInt(newTime) + 60000; //increment 1 minute
+                }
+
+                // console.log(newjson);
+
+                // Set our internal DB variable
+                var db = req.db;
+
+                // Set our collection
+                var collection = db.get('DormEnergyUsage');
+
+                // Submit to the DB
+                collection.insert(newjson, function (err, doc) {
+                    if (err) {
+                        // If it failed, return error
+                        res.send("There was a problem adding the information to the database.");
+                    }
+                    else {
+                        // And forward to success page
+                        // res.redirect("chapin");
+                    }
+                });
+
             });
 
         });
