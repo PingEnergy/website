9a2a895b163241a537f669345886da75837851a6
Jingxian <jingxian.liu@gmail.com>
2016-04-29 21:02:56 -0400

    style

--- PingEnergy/routes/api.js
+++ PingEnergy/routes/api.js
@@ -12,20 +12,20 @@ router.get('/:building', cache('10 minutes'), function(req, res, next) {
     //energyUsage
     // request('http://cs.wheatoncollege.edu/~egauge/' + building + '/'  + building + '.xml',
     //energyUsagePerDay
-    // request('http://egauge-clark-mcintire-young.wheatoncollege.edu/cgi-bin/egauge-show?d' ,
+    // request('http://egauge-beard.wheatoncollege.edu/cgi-bin/egauge-show?d' ,
 
         // function (error, response, body) {
         //     var parseString = xml2js.parseString;
         //     var xml = body;
         //
-        //     parseString(xml,
-        //     function (err, result) {
+            // parseString(xml,
+            // function (err, result) {
                 // 1kwh = .6379 pounds of co2
                 // 2000 pounds of co2 = 5 trees
                 // 400 pounds of co2 = 1 tree
                 // 400 / 1 = .6379 / x ~> .00159 tree per kwh
 
-                // var newjson = {"building": "Young",
+                // var newjson = {"building": "Beard",
                 //             "startTime": 1,
                 //             "endTime": 5,
                 //             "timeInterval": 60,
@@ -54,17 +54,19 @@ router.get('/:building', cache('10 minutes'), function(req, res, next) {
 
                 // energyUsagePerDay
                 // newjson["endTime"] = parseInt(result["group"]["data"][0]["$"]["time_stamp"], 16) * 1000;
-                // newjson["startTime"] = newjson["endTime"]-691200000;
+                // newjson["startTime"] = newjson["endTime"] - 4579200000;
                 // newjson["timeInterval"] = result["group"]["data"][0]["$"]["time_delta"];
-                // var newTime = newjson["endTime"]-691200000;
-                // for (var i = 8; i>=0; i--){
+                // var newTime = newjson["endTime"] - 4579200000;
+                // for (var i=53; i>=0; i--){
                 //      newTime = newTime.toString();
                 //      newEnergy = {};
                 //      newCO2 = {};
                 //      newTree = {};
-                //      newEnergy[newTime] = result["group"]["data"][0]["r"][i]["c"][2];
+                //      newEnergy[newTime] = result["group"]["data"][0]["r"][i]["c"][0];
+                //      console.log(newTime);
+                //      console.log(newEnergy[newTime]);
                 //      newjson["energyUsage"].push( newEnergy );
-                //      newCO2[newTime] = result["group"]["data"][0]["r"][i]["c"][2]*0.6379;
+                //      newCO2[newTime] = result["group"]["data"][0]["r"][i]["c"][0]*0.6379;
                 //      newjson["co2"].push(newCO2);
                 //      newTree[newTime] = newEnergy[newTime] *0.00159;
                 //      newjson["treeOffset"].push(newTree);
@@ -78,23 +80,23 @@ router.get('/:building', cache('10 minutes'), function(req, res, next) {
 //
 //
 //                 // Set our internal DB variable
-//                 var db = req.db;
+                // var db = req.db;
 //
 //                 // Set our collection
-//                 var collection = db.get('DormEnergyPerDay');
+                // var collection = db.get('DormEnergyPerDay');
 
                 // Submit to the DB
-//                 collection.insert(newjson, function (err, doc) {
-//                     if (err) {
-//                         // If it failed, return error
-//                         res.send("There was a problem adding the information to the database.");
-//                         console.log(err);
-//                     }
-//                     else {
-//                         // And forward to success page
-//                         console.log("DormEnergyUsagePerDay: success!");
-//                     }
-//                 });
+                // collection.insert(newjson, function (err, doc) {
+                //     if (err) {
+                //         // If it failed, return error
+                //         res.send("There was a problem adding the information to the database.");
+                //         console.log(err);
+                //     }
+                //     else {
+                //         // And forward to success page
+                //         console.log("DormEnergyUsagePerDay: success!");
+                //     }
+                // });
 
 
 //                 newjson["energyUsage"] = [];
